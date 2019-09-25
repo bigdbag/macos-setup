137d1b2713eb5c36899d63aa1bb165c4fdf7cdd4
bigdbag <sprintforyourlife@gmail.com>
2019-09-25 13:04:31 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -40,6 +40,13 @@ echo "Installing node and npm"
 
 brew install node
 
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+nvm install-latest-npm
+
 echo "------------------------------"
 echo "Installing font tools"
 
